2ffa4bc5459bfe26a8dfc0e17fc6aa2a20850780
Your Name <you@example.com>
2025-03-24 09:32:36 +0530

    push aggain

--- src/app.js
+++ src/app.js
@@ -3,18 +3,20 @@ const connectDB = require("./config/database");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const app = express();
-
-const HOST = "51.21.3.185"; // Change from localhost to your server IP
-const PORT = 3000;
+// const http = require("http");
 
 // CORS Configuration
 app.use(cors({
-    origin: "http://51.21.3.185:5173", // Update frontend origin
+    origin: "http://localhost:5173",
+    // methods: ["GET", "POST", "PATCH", "DELETE"],
     credentials: true,
+    // allowedHeaders: ["Content-Type", "Authorization"],
 }));
 
+// app.options("*", cors());  
 // Middleware
 app.use(express.json());
+// app.use(express.urlencoded({ extended: true })); 
 app.use(cookieParser());
 
 // Import Routes
@@ -33,8 +35,8 @@ app.use("/", userRouter);
 connectDB()
     .then(() => {
         console.log("Database connection established");
-        app.listen(PORT, HOST, () => {
-            console.log(`Server is successfully running on http://${HOST}:${PORT}`);
+        app.listen(3000, () => {
+            console.log("Server is successfully running on http://localhost:3000");
         });
     })
     .catch((err) => {
